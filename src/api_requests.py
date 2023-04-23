faac7fe9c87cc7807f0f8f9757ff7ff0714acaff
Lukas Scholtes <luke.scholtes@gmail.com>
2023-04-23 18:13:45 +0100

    Handle 500s

--- src/api_requests.py
+++ src/api_requests.py
@@ -1,5 +1,7 @@
 import configparser
 import json
+import logging
+import time
 from typing import List
 
 from requests import request
@@ -11,6 +13,9 @@ config.read("config.ini")
 
 API_KEY = config["DEFAULT"]["API_KEY"]
 BASE_URL = config["DEFAULT"]["BASE_URL"]
+MAX_RETRIES = config["DEFAULT"]["MAX_RETRIES"]
+
+logger = logging.getLogger()
 
 
 class ResponseException(Exception):
@@ -20,7 +25,7 @@ class ResponseException(Exception):
         super().__init__(f"Status code {self.status_code}: {self.message}")
 
 
-def get_outages() -> List[Outage]:
+def get_outages(retry: int = 0) -> List[Outage]:
     r = request(
         method="GET",
         url=f"{BASE_URL}/outages",
@@ -29,10 +34,14 @@ def get_outages() -> List[Outage]:
     all_outages = json.loads(r.text)
     if r.status_code == 200:
         return [Outage(**outage) for outage in all_outages]
+    elif r.status_code == 500 and retry < MAX_RETRIES:
+        logger.warn(f"500 encountered in GET outages on {retry=}, retrying...")
+        time.sleep(3)
+        return get_outages(retry + 1)
     raise ResponseException(status_code=r.status_code, message=r.text)
 
 
-def get_site_info(site_id: str) -> Site:
+def get_site_info(site_id: str, retry: int = 0) -> Site:
     r = request(
         method="GET",
         url=f"{BASE_URL}/site-info/{site_id}",
@@ -41,6 +50,10 @@ def get_site_info(site_id: str) -> Site:
     site_info = json.loads(r.text)
     if r.status_code == 200:
         return Site(**site_info)
+    elif r.status_code == 500 and retry < MAX_RETRIES:
+        logger.warn(f"500 encountered in GET site-info on {retry=}, retrying...")
+        time.sleep(3)
+        return get_site_info(site_id, retry + 1)
     raise ResponseException(status_code=r.status_code, message=r.text)
 
 
@@ -48,7 +61,7 @@ def _create_json_outages_payload(outages: List[Outage]) -> str:
     return json.dumps([outage.to_POST_dict() for outage in outages])
 
 
-def post_outages(outages: List[Outage], site: Site) -> bool:
+def post_outages(outages: List[Outage], site: Site, retry: int = 0) -> bool:
     json_payload = _create_json_outages_payload(outages)
     r = request(
         method="POST",
@@ -58,4 +71,8 @@ def post_outages(outages: List[Outage], site: Site) -> bool:
     )
     if r.status_code == 200:
         return True
+    elif r.status_code == 500 and retry < MAX_RETRIES:
+        logger.warn(f"500 encountered in GET site-info on {retry=}, retrying...")
+        time.sleep(3)
+        return post_outages(outages, site, retry + 1)
     raise ResponseException(status_code=r.status_code, message=r.text)
