9ecb00787452711cacd4256fab70754d18d5e6c2
Lukas Scholtes <luke.scholtes@gmail.com>
2023-04-23 17:47:27 +0100

    API request logic

---
+++ api_requests.py
@@ -0,0 +1,61 @@
+import configparser
+import json
+from typing import List
+
+from requests import request
+
+from models import Outage, Site
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+API_KEY = config["DEFAULT"]["API_KEY"]
+BASE_URL = config["DEFAULT"]["BASE_URL"]
+
+
+class ResponseException(Exception):
+    def __init__(self, status_code: int, message: str):
+        self.status_code = status_code
+        self.message = message
+        super().__init__(f"Status code {self.status_code}: {self.message}")
+
+
+def get_outages() -> List[Outage]:
+    r = request(
+        method="GET",
+        url=f"{BASE_URL}/outages",
+        headers={"x-api-key": API_KEY},
+    )
+    all_outages = json.loads(r.text)
+    if r.status_code == 200:
+        return [Outage(**outage) for outage in all_outages]
+    raise ResponseException(status_code=r.status_code, message=r.text)
+
+
+def get_site_info(site_id: str) -> Site:
+    r = request(
+        method="GET",
+        url=f"{BASE_URL}/site-info/{site_id}",
+        headers={"x-api-key": API_KEY},
+    )
+    site_info = json.loads(r.text)
+    if r.status_code == 200:
+        return Site(**site_info)
+    raise ResponseException(status_code=r.status_code, message=r.text)
+
+
+def _create_json_outages_payload(outages: List[Outage]) -> str:
+    return json.dumps([outage.to_POST_dict() for outage in outages])
+
+
+def post_outages(outages: List[Outage], site: Site) -> bool:
+    json_payload = _create_json_outages_payload(outages)
+    r = request(
+        method="POST",
+        url=f"{BASE_URL}/site-outages/{site.id}",
+        data=json_payload,
+        headers={"x-api-key": API_KEY},
+    )
+    if r.status_code == 200:
+        return True
+    raise ResponseException(status_code=r.status_code, message=r.text)
---
+++ test_api_requests.py
@@ -0,0 +1,24 @@
+from api_requests import _create_json_outages_payload
+from models import Outage
+
+OUTAGES = [
+    {
+        "id": "002b28fc-283c-47ec-9af2-ea287336dc1b",
+        "begin": "2021-07-26T17:09:31.036Z",
+        "end": "2021-08-29T00:37:42.253Z",
+        "name": "foo",
+    },
+    {
+        "id": "002b28fc-283c-47ec-9af2-ea287336dc1b",
+        "begin": "2022-05-23T12:21:27.377Z",
+        "end": "2022-11-13T02:16:38.905Z",
+        "name": "bar",
+    },
+]
+
+
+def test_create_json_outages_payload():
+    outages = [Outage(**outage_kwargs) for outage_kwargs in OUTAGES]
+    json_outages_payload = _create_json_outages_payload(outages)
+    expected_payload = '[{"id": "002b28fc-283c-47ec-9af2-ea287336dc1b", "begin": "2021-07-26T17:09:31.036Z", "end": "2021-08-29T00:37:42.253Z", "name": "foo"}, {"id": "002b28fc-283c-47ec-9af2-ea287336dc1b", "begin": "2022-05-23T12:21:27.377Z", "end": "2022-11-13T02:16:38.905Z", "name": "bar"}]'
+    assert json_outages_payload == expected_payload
